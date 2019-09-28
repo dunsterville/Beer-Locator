17c31d75a171d0a9061f81e05bd88350163e14ce
thejunglife <thejunglife@gmail.com>
2019-09-28 10:32:25 -0700

    implement geoApi to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,12 @@ let cityname //pass a city name into this var later.
     console.log(data)
   })
   .catch(err => {console.log(err)})
+
+let latitude // will recieve from brewery api
+let longitude // will recieve from brewery api
+fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=d62fec397ede46c28c554363b73c4563`)
+  .then(r => r.json())
+  .then(data => {
+    console.log (data)
+  })
+  .catch(err => console.log(err))
