c79d8556b2a319423018a6390ad059d33eaa7ad0
David Wu <davidjwu92@gmail.com>
2019-09-28 10:44:27 -0700

    Navbar completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,15 @@
-let cityname //pass a city name into this var later.
+//for Materialize's responsive navbar
+$(document).ready(function(){ //When the document's loaded, it'll be ready for menu icon click
+    $('.sidenav').sidenav()
+})
+
+//OpenBrewery fetch request
+let cityname //pass a city name into this var later?
   fetch(`https://api.openbrewerydb.org/breweries?by_city=${cityname}`)
   .then(r => r.json())
   .then(data => {
     console.log(data)
   })
   .catch(err => {console.log(err)})
+
+  
